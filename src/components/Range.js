e4d740c0f7ff99601272be36197cfd657e51290a
Abhas Tandon <abhastandon007@gmail.com>
2015-11-21 21:43:26 -0800

    Implement optional for num range

--- src/components/Range.js
+++ src/components/Range.js
@@ -13,10 +13,17 @@ export default React.createClass({
             sortedProp = sortedProp.filter(function(o){
                 return typeof o !== "undefined";
             });
+
             this.min = sortedProp[0];
             this.max = sortedProp[sortedProp.length - 1];
+
+            let filterEventDetailsObj = this.filterEventDetails([this.min, this.max], !this.props.optional);
+            let event = new CustomEvent('filterChange', filterEventDetailsObj);
+            document.dispatchEvent(event);
+
             return {
-                start: [this.min, this.max]
+                start: [this.min, this.max],
+                disabled: this.props.optional
             };
         },
         componentWillMount() {
@@ -31,30 +38,58 @@ export default React.createClass({
             start = start.map(s => parseInt(s));
             this.setState({start});
             let component = this;
-            let event = new CustomEvent('filterChange', {
+            let filterEventDetailsObj = this.filterEventDetails(start, true);
+            let event = new CustomEvent('filterChange', filterEventDetailsObj);
+            document.dispatchEvent(event);
+        },
+
+        filterEventDetails(val, enabled) {
+            let component = this;
+            return {
                 detail: {
                     propName: component.props.propname,
+                    enabled: enabled,
                     filterFunction: function(data) {
                         return data.filter(function(d) {
-                            return d[component.props.propname] >= start[0] && d[component.props.propname] <= start[1];
+                            return d[component.props.propname] >= val[0] && d[component.props.propname] <= val[1];
                         });
                     }
                 }
+            };
+        },
+
+        toggleFilter(e) {
+            this.setState(prevState => {
+                prevState.disabled = !e.target.checked;
+                return prevState;
             });
+            let filterEventDetailsObj = this.filterEventDetails(this.state.start, e.target.checked);
+            let event = new CustomEvent('filterChange', filterEventDetailsObj);
             document.dispatchEvent(event);
         },
 
         render() {
-            return ( <Nouislider range = {
-                    {
-                        min: this.min,
-                        max: this.max
-                    }
-                }
-                step = {1}
-                start = {this.state.start}
-                onChange = {this.rangeChangeHandler}
-                tooltips />
+            let filterSwitch = '';
+            if (this.props.optional) {
+                filterSwitch = <input type='checkbox' name='filterState' checked={!this.state.disabled} onChange={this.toggleFilter} />
+            }
+            let style = {display: (this.state.disabled) ? 'none' : 'block'};
+            return ( 
+                <div>
+                    {filterSwitch}
+                    <div style = {style}>
+                        <Nouislider 
+                            range = {{
+                                        min: this.min,
+                                        max: this.max
+                                    }}
+                            step = {1}
+                            start = {this.state.start}
+                            onChange = {this.rangeChangeHandler}
+                            disabled={this.state.disabled}
+                            tooltips/>
+                    </div>
+                </div>
             );
         }
 });
